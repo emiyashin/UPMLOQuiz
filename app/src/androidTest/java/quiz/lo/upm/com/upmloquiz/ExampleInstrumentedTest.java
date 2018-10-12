de74a6cf7d3dbbef3651ada2504d610689373621
emiyashin <182556@student.upm.edu.my>
2018-10-12 12:10:16 +0800

    First edited release with Firebase login register system.

---
+++ app/src/androidTest/java/quiz/lo/upm/com/upmloquiz/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package quiz.lo.upm.com.upmloquiz;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("quiz.lo.upm.com.upmloquiz", appContext.getPackageName());
+    }
+}
