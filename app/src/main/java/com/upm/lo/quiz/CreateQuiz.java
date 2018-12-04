c583b88a51851086a84e22ef9df0ca160b38d671
emiyashin <182556@student.upm.edu.my>
2018-12-04 23:46:32 +0800

    https://www.youtube.com/watch?v=EzGz6W_AGNk&list=PLF0BIlN2vd8tJjVKdNegtQF2j9CjmGOc_&index=4
              Version 1.

--- app/src/main/java/com/upm/lo/quiz/CreateQuiz.java
+++ app/src/main/java/com/upm/lo/quiz/CreateQuiz.java
@@ -1,11 +1,12 @@
 package com.upm.lo.quiz;
 
-import android.renderscript.Sampler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,51 +20,68 @@ import java.util.Map;
 
 public class CreateQuiz extends AppCompatActivity {
 
-    private EditText cQuestion, cOption1, cOption2, cOption3, cOption4, typeLO;
-
-
+    private Button cPush;
+    private EditText cQuestion, cOption1, cOption2, cOption3, cOption4, cAnswer, typeLO;
+    public long questionCount;
 
     DatabaseReference cReference;
-    public long questionCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_quiz);
 
-        cReference = FirebaseDatabase.getInstance().getReference().child("Questions");
-
         cQuestion = (EditText) findViewById(R.id.cvQuestion);
         cOption1 = (EditText) findViewById(R.id.cvOption1);
         cOption2 = (EditText) findViewById(R.id.cvOption2);
         cOption3 = (EditText) findViewById(R.id.cvOption3);
         cOption4 = (EditText) findViewById(R.id.cvOption4);
+        cAnswer = (EditText) findViewById(R.id.cvAnswer);
         typeLO = (EditText) findViewById(R.id.cvTypeLO);
 
-        DatabaseReference newPostRef = cReference.push();
-        newPostRef.setValue();
+        cPush = (Button) findViewById(R.id.cButton);
 
-        cReference.addValueEventListener(new ValueEventListener() {
-             @Override
-             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                questionCount = dataSnapshot.getChildrenCount();
-
-                 }
-
-             @Override
-             public void onCancelled(@NonNull DatabaseError databaseError) {
-
-             }
-         });
-
-                Map<Integer, Question> createQuiz = new HashMap<>();
-                createQuiz.put((int)questionCount+1, new Question(cQuestion, "option1", "option2", "option3", "option4", "answer", 0));
-
-
-
-        cReference.updateChildrenAsync(userUpdates);
-
-
+        createQuiz();
 
     }
+
+    private void createQuiz() {
+
+        cReference = FirebaseDatabase.getInstance().getReference().child("Questions");
+
+        cPush.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String cQuestion = ((EditText) findViewById(R.id.cvQuestion)).getText().toString();
+                String cOption1 = ((EditText) findViewById(R.id.cvOption1)).getText().toString();
+                String cOption2 = ((EditText) findViewById(R.id.cvOption2)).getText().toString();
+                String cOption3 = ((EditText) findViewById(R.id.cvOption3)).getText().toString();
+                String cOption4 = ((EditText) findViewById(R.id.cvOption4)).getText().toString();
+                String cAnswer = ((EditText) findViewById(R.id.cvAnswer)).getText().toString();
+                String typeLO = ((EditText) findViewById(R.id.cvTypeLO)).getText().toString();
+
+                int intTypeLO = Integer.parseInt(typeLO);
+
+                Map<Integer, Question> createQuiz = new HashMap<>();
+                createQuiz.put((int)questionCount+1, new Question(cQuestion, cOption1, cOption2, cOption3, cOption4, cAnswer,intTypeLO));
+                DatabaseReference newCRef = cReference.child("Question").push();
+                newCRef.setValue(createQuiz);
+                Toast.makeText(getApplicationContext(), "Question uploaded.", Toast.LENGTH_SHORT).show();
+            }
+
+    });
+
+        cReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                questionCount = dataSnapshot.getChildrenCount();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/upm/lo/quiz/MainActivity.java
+++ app/src/main/java/com/upm/lo/quiz/MainActivity.java
@@ -19,7 +19,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private Button btnChangeEmail, btnChangePassword, btnSendResetEmail, btnRemoveUser,
-            changeEmail, changePassword, sendEmail, remove, signOut, btnQuiz;
+            changeEmail, changePassword, sendEmail, remove, signOut, btnQuiz, btnCreate;
 
     private EditText oldEmail, newEmail, password, newPassword;
     private ProgressBar progressBar;
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
         remove = (Button) findViewById(R.id.remove);
         signOut = (Button) findViewById(R.id.sign_out);
         btnQuiz = (Button) findViewById(R.id.quizActivity);
+        btnCreate = (Button) findViewById(R.id.createActivity);
 
         oldEmail = (EditText) findViewById(R.id.old_email);
         newEmail = (EditText) findViewById(R.id.new_email);
@@ -244,6 +245,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, QuizActivity.class));
+            }
+        });
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CreateQuiz.class));
             }
         });
 
