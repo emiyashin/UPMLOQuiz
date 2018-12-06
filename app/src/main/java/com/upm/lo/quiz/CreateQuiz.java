8fc338d5d40d10617aec17c55e3c5478f5a0e990
emiyashin <182556@student.upm.edu.my>
2018-12-06 22:20:40 +0800

    v5.6.1

--- app/src/main/java/com/upm/lo/quiz/CreateQuiz.java
+++ app/src/main/java/com/upm/lo/quiz/CreateQuiz.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -22,6 +23,7 @@ public class CreateQuiz extends AppCompatActivity {
 
     private Button cPush;
     private EditText cQuestion, cOption1, cOption2, cOption3, cOption4, cAnswer, typeLO;
+    private TextView testCounter;
     public long questionCount;
 
     DatabaseReference cDatabase;
@@ -41,6 +43,8 @@ public class CreateQuiz extends AppCompatActivity {
 
         cPush = (Button) findViewById(R.id.cButton);
 
+        testCounter = (TextView) findViewById(R.id.tQuestion);
+
         createQuiz();
 
     }
@@ -50,62 +54,69 @@ public class CreateQuiz extends AppCompatActivity {
         //FirebaseDatabase database = FirebaseDatabase.getInstance();
         //final DatabaseReference cReference = database.getReference("Questions");
 
-        int qCount = (int)(questionCount)+1;
-        String counter = Integer.toString(qCount);
+        //int qCount = (int)(questionCount)+1;
+        //final String counter = Integer.toString(qCount);
 
-        cDatabase = FirebaseDatabase.getInstance().getReference("Questions/"+counter+"/");
+         //String counter = Integer.toString((int)(questionCount)+1);
 
-        cPush.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                String question = cQuestion.getText().toString();
-                String option1 = cOption1.getText().toString();
-                String option2 = cOption2.getText().toString();
-                String option3 = cOption3.getText().toString();
-                String option4 = cOption4.getText().toString();
-                String answer = cAnswer.getText().toString();
-                String LO = typeLO.getText().toString();
 
-                //int intTypeLO = Integer.parseInt(LO);
-
-                int qCount = (int)(questionCount)+1;
-                String counter = Integer.toString(qCount);
-
-                Map<String, String> createQuiz = new HashMap<>();
-
-                createQuiz.put("question", question);
-                createQuiz.put("option1", option1);
-                createQuiz.put("option2", option2);
-                createQuiz.put("option3", option3);
-                createQuiz.put("option4", option4);
-                createQuiz.put("answer", answer);
-                createQuiz.put("intLO", LO);
-                createQuiz.put("questionCount", counter);
-
-                //createQuiz.put(counter, new Question(question, option1, option2, option3, option4, answer,intTypeLO));
-
-                //createQuiz.put("Question number", counter)
-                //createQuiz.put(counter, new Question(question, option1, option2, option3, option4, answer,intTypeLO));
-
-                //DatabaseReference newCRef = cDatabase.child("Questions").push();
-                //newCRef.setValue(createQuiz);
-
-                cDatabase.setValue(createQuiz);
-
-                Toast.makeText(getApplicationContext(), "Question uploaded.", Toast.LENGTH_SHORT).show();
-            }
-
-    });
-
+        cDatabase = FirebaseDatabase.getInstance().getReference("Questions");
         cDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 questionCount = dataSnapshot.getChildrenCount();
+
+                //int intTypeLO = Integer.parseInt(LO);
+
+                int qCount = (int)(questionCount)+1;
+                final String counter = Integer.toString(qCount);
+                testCounter.setText(counter);
+
+
+                cPush.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        //createQuiz.put(counter, new Question(question, option1, option2, option3, option4, answer,intTypeLO));
+
+                        //createQuiz.put("Question number", counter)
+                        //createQuiz.put(counter, new Question(question, option1, option2, option3, option4, answer,intTypeLO));
+
+                        //DatabaseReference newCRef = cDatabase.child("Questions").push();
+                        //newCRef.setValue(createQuiz);
+
+                        String question = cQuestion.getText().toString();
+                        String option1 = cOption1.getText().toString();
+                        String option2 = cOption2.getText().toString();
+                        String option3 = cOption3.getText().toString();
+                        String option4 = cOption4.getText().toString();
+                        String answer = cAnswer.getText().toString();
+                        String LO = typeLO.getText().toString();
+
+                        Map<String, String> createQuiz = new HashMap<>();
+
+                        createQuiz.put("question", question);
+                        createQuiz.put("option1", option1);
+                        createQuiz.put("option2", option2);
+                        createQuiz.put("option3", option3);
+                        createQuiz.put("option4", option4);
+                        createQuiz.put("answer", answer);
+                        createQuiz.put("intLO", LO);
+                        //createQuiz.put("questionCount", counter);
+
+                        cDatabase.child(counter).setValue(createQuiz);
+                        Toast.makeText(getApplicationContext(), "Question uploaded.", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                });
+
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(), "Question failed to upload.", Toast.LENGTH_SHORT).show();
 
             }
         });
