385663585d42b5e7212d61ca4f23b7640b55a81f
emiyashin <yuuri_45@yahoo.com>
2018-12-12 14:59:18 +0800

    version 5.8, remove quiz function using spinner

--- app/src/main/java/com/upm/lo/quiz/RemoveActivity.java
+++ app/src/main/java/com/upm/lo/quiz/RemoveActivity.java
@@ -1,17 +1,11 @@
 package com.upm.lo.quiz;
 
-import android.database.Cursor;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -26,8 +20,6 @@ public class RemoveActivity extends AppCompatActivity {
 
     private Spinner quizSpin;
     private Button removeBtn;
-    private TextView test;
-    private String spinText;
 
     DatabaseReference cDatabase;
 
@@ -38,8 +30,7 @@ public class RemoveActivity extends AppCompatActivity {
         setContentView(R.layout.activity_remove);
 
         final Spinner quizSpin = (Spinner) findViewById(R.id.questionSpinner);
-        final Button removeBtn = (Button) findViewById(R.id.removeQuiz);
-        final TextView test = (TextView) findViewById(R.id.qTest);
+        Button removeBtn = (Button) findViewById(R.id.removeQuiz);
 
         cDatabase = FirebaseDatabase.getInstance().getReference("Questions");
 
@@ -50,30 +41,15 @@ public class RemoveActivity extends AppCompatActivity {
                 // Is better to use a List, because you don't know the size
                 // of the iterator returned by dataSnapshot.getChildren() to
                 // initialize the array
-                final List<String> questionList = new ArrayList<String>();
+                final List<String> questionsNo = new ArrayList<String>();
 
-                for (DataSnapshot questionSnapshot: dataSnapshot.getChildren()) {
-                    String questions = questionSnapshot.child("questionCount").getValue(String.class);
-                    if (questions!=null){
-                        questionList.add(questions);
-                    }
+                for (DataSnapshot areaSnapshot: dataSnapshot.getChildren()) {
+                    String areaName = areaSnapshot.child("questionCount").getValue(String.class);
+                    questionsNo.add(areaName);
                 }
-
-                ArrayAdapter<String> questionAdapter = new ArrayAdapter<String>(RemoveActivity.this, android.R.layout.simple_spinner_item, questionList);
+                ArrayAdapter<String> questionAdapter = new ArrayAdapter<String>(RemoveActivity.this, android.R.layout.simple_spinner_item, questionsNo);
                 questionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 quizSpin.setAdapter(questionAdapter);
-
-                removeBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        cDatabase.child(spinText).removeValue();
-                        Toast.makeText(getApplicationContext(), "Question "+spinText+" deleted.", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                //quizSpin.setOnItemClickListener(onItemSelectedListener1);
-
-                //String spinText = quizSpin.getSelectedItem().toString();
 
             }
 
@@ -83,23 +59,6 @@ public class RemoveActivity extends AppCompatActivity {
             }
         });
 
-        quizSpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                spinText = quizSpin.getSelectedItem().toString();
-                test.setText(spinText);
-            }
-                @Override
-                public void onNothingSelected(AdapterView<?> parent){
-
-        }
-
-
-
-    });
-
 
     }
-
-
 }
